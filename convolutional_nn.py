975408e8728778877419a61d80566e67fcd9b86f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 17:46:25 +0000

    Adding tarfile member sanitization to extractall()

--- convolutional_nn.py
+++ convolutional_nn.py
@@ -15,7 +15,26 @@ download_url(dataset_url, '.')
 
 # extract from archive
 with tarfile.open('./cifar10.tgz', 'r:gz') as tar:
-    tar.extractall(path='./data')
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar, path="./data")
 
 
 data_dir = './data/cifar10'
